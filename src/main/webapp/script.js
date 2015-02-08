7845872044fd2cd85c0a18211266f340be5ae77f
liisa <liisa.sailaranta@aalto.fi>
2015-02-08 22:08:34 +0200

    Commenting code

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -11,31 +11,38 @@ if (window.location.hostname == "localhost") {
 else {
 	app.url = "http://thawing-journey-9067.herokuapp.com/Reservations/";
 }
-app.months = ["Tammikuu", "Helmikuu", "Maaliskuu", "Huhtikuu", "Toukokuu",
-              "Kesakuu", "Heinakuu", "Elokuu", "Syyskuu", "Lokakuu", "Marraskuu", "Joulukuu"];
 
 
 app.controller("MyController", function($scope, $http) {
-	// The selected date
+	// The selected date in the scope
 	$scope.d = new Date();
     
+	// The static list of months
+	$scope.months = ["Tammikuu", "Helmikuu", "Maaliskuu", "Huhtikuu", "Toukokuu",
+	                  "Kesakuu", "Heinakuu", "Elokuu", "Syyskuu", "Lokakuu", "Marraskuu", "Joulukuu"];
+	
+	// The static list of weekday names
 	$scope.weekDays= ["Ma", "Ti", "Ke", "To", "Pe", "La", "Su"];
 	
-	// Apartment information. Need to re-check after getting actual information.
+	// Static partment information. Need to re-check after getting actual information.
 	$scope.steps = ["A", "B"];
 	$scope.aApartments = ["1", "2", "3"];
 	$scope.bApartments = ["1", "2", "3", "4"];
 	
+	// Fetch month days based on the currently selected date.
 	$scope.updateMonth = function() {
-	
+	    
+		// Increase month by one. Note js months start from 0.
 		var m = $scope.d.getMonth() + 1;
 		var y = $scope.d.getFullYear();
 
-		var firstDayOfMonth = new Date(y, m - 1, 1, 1, 0, 0, 0);
-		
+	    // Which weekday to start the montt with
+		var firstDayOfMonth = new Date(Date.UTC(y, m - 1, 1, 1, 0, 0, 0));
 		var weekDayOfFirstDay = firstDayOfMonth.getDay();
-		// Lets start days from Monday
+		// Lets start week from Monday
 		weekDayOfFirstDay = (weekDayOfFirstDay + 6) % 7;
+		
+		// Calculate how many days there are in current month
 		var daysInMonth;
 		// February
 		if (m == 2) {
@@ -65,17 +72,20 @@ app.controller("MyController", function($scope, $http) {
 		else {
 			daysInMonth = 31;
 		}
-		var i = 1;
+		
 		var weeks = [];
-		//TODO check the loop and select the current date! use $scope.d!!!
-		while (i < daysInMonth + 1) {
+		// Put month days to weeks starting from 1.
+		var i = 1;
+		while (i <= daysInMonth) {
 			var week = [];
-			// init days to zero
+			// Init all days to zero. Empty days are represented by zero.
 			for (var k = 0; k < 7; k++) {
 				week[k] = {number : 0, month: 0, year: 0};
 			}
-			for (var j = weekDayOfFirstDay; j < 7 && i < daysInMonth + 1; j++) {
+			// Fill the week starting from the weekday of the first day.
+			for (var j = weekDayOfFirstDay; j < 7 && i <= daysInMonth; j++) {
 				week[j] = {number: i, month : m, year : y};
+				// Pre-select the current date
 				if ($scope.d.getDate() == i) {
 					week[j]["selected"] = true;
 					$scope.selectedDay = week[j];
@@ -85,10 +95,40 @@ app.controller("MyController", function($scope, $http) {
 			weekDayOfFirstDay = 0;
 			weeks[weeks.length] = week;
 		}
-		$scope.monthName = app.months[m - 1];
+		// Set the month name.
+		$scope.monthName = $scope.months[m - 1];
 		$scope.month = weeks;
 
 	};
+	
+	$scope.pickDay = function(day) {
+		// Update calender view.
+		$scope.selectedDay.selected = false;
+		day.selected = true;
+		$scope.selectedDay = day;
+		// Update date information.
+		$scope.d = new Date(Date.UTC(day.year, day.month - 1, day.number, 1, 0, 0, 0));
+		$scope.fetchData();
+	};
+
+	$scope.nextMonth = function () {
+		var month = $scope.d.getMonth();
+		// Increase month by one
+		$scope.d = new Date(Date.UTC( $scope.d.getFullYear(), month + 1, 1, 1, 0, 0, 0));
+		$scope.updateMonth();
+		$scope.fetchData();
+
+	};
+	
+	$scope.previousMonth = function () {
+		var month = $scope.d.getMonth();
+		// Decrease month by one
+		$scope.d = new Date(Date.UTC( $scope.d.getFullYear(), month - 1, 1, 1, 0, 0, 0));
+		$scope.updateMonth();
+		$scope.fetchData();
+
+	};
+	// Toggle reservation selection.
 	$scope.toggleReservation = function(reservation) {
 		if (!reservation.reserved) {
 			if (reservation.reserved == "selected") {
@@ -100,32 +140,7 @@ app.controller("MyController", function($scope, $http) {
 		}
 		
 	};
-	$scope.nextMonth = function () {
-		var month = $scope.d.getMonth();
-		// Increase month by one
-		$scope.d = new Date(Date.UTC( $scope.d.getFullYear(), month + 1, 1, 1, 0, 0, 0));
-		$scope.updateMonth();
-		$scope.fetchData();
-
-	};
-	$scope.previousMonth = function () {
-		var month = $scope.d.getMonth();
-		// Increase month by one
-		$scope.d = new Date(Date.UTC( $scope.d.getFullYear(), month - 1, 1, 1, 0, 0, 0));
-		$scope.updateMonth();
-		$scope.fetchData();
-
-	};
-	
-	
-	$scope.pickDay = function(day) {
-		$scope.selectedDay.selected = false;
-		day.selected = true;
-		$scope.selectedDay = day;
-		$scope.d = new Date(Date.UTC(day.year, day.month - 1, day.number, 1, 0, 0, 0));
-		$scope.fetchData();
-	};
-	
+	// Fetch reservations related to selected date.
 	$scope.fetchData = function() {
 		var url =  app.url + $scope.d.getTime();
 		$http.get(url).success(function(data) {
@@ -137,13 +152,16 @@ app.controller("MyController", function($scope, $http) {
 		});
 	};
 	
-	$scope.update = function(user) {
+	// Save the reservation made by the user.
+	$scope.saveReservation = function(user) {
 		for (var i = 0;  i< $scope.data.length; i++) {
 			var reservation = $scope.data[i];
 			if (reservation.reserved == "selected") {
-				reservation.reserved = true;
+				// Set the user data.
 				reservation.step = user.step;
 				reservation.apartmentNumber = user.apartment;
+				
+				reservation.reserved = true;
 				$http.put(app.url+reservation.id, reservation).success(function(data) {
 				
 				});
